f0d24a31b1f858ac70c90701887e8aa7e5568a01
BaoChau478 <chau47778@gmail.com>
2026-02-08 17:28:11 +0700

    add header

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
-import React from 'react'
-import Background from './layout/background'
+import Header from './components/layouts/header/Header'
+import Background from './layout/Background'
 
 export default function App() {
   return (
     <div className='margin:0, padding:0'>
+      <Header/>
       <Background/>
     </div>
   )
